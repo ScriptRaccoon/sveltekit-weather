ce10924bac0d19cde87710df39cd1fe038adfaea
Script Raccoon <scriptraccoon@gmail.com>
2022-06-12 12:21:17 +0200

    env handling

--- src/routes/search/index.js
+++ src/routes/search/index.js
@@ -1,9 +1,10 @@
 import { getName } from "country-list";
-const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
-const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function get(event) {
+    const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+    const BASE_URL =
+        "https://api.openweathermap.org/data/2.5/weather";
     try {
         const city = event.url.searchParams.get("city");
         const url = `${BASE_URL}?q=${city}&units=metric&appid=${API_KEY}`;
